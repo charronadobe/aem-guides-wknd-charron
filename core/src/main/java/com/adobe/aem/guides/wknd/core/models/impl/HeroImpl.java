cee188a1538c713f416cc9f8a1b33b06f472b53b
Andy Charron <charron@adobe.com>
2021-12-23 14:25:48 -0500

    build successful, still overwrites content

--- core/src/main/java/com/adobe/aem/guides/wknd/core/models/impl/HeroImpl.java
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/impl/HeroImpl.java
@@ -6,6 +6,7 @@ import javax.inject.Inject;
 import com.adobe.aem.guides.wknd.core.models.Hero;
 
 import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.models.annotations.Default;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
