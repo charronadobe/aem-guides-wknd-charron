25387ec64ae84bc423bb21c4856ba642a0c21bd9
Andy Charron <charron@adobe.com>
2022-02-22 10:58:43 -0500

    remove blue jays themed source

--- blue_jays-theme-sources/src/theme.ts
+++
@@ -1,9 +0,0 @@
-
-// Stylesheets
-import "./theme.scss";
-import "./resources/images/favicon.png"
-
-// Component Javascript
-import "./components/experiencefragment/change_flag.js";
-import "./components/form-options/form_dropdown.js";
-import "./components/progressbar/progressbar.js";
